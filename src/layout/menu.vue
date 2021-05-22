947cf45bde76382ee4e9b01bd83979c1adb5ca31
xinshaoguang <997888255@qq.com>
2021-05-22 13:36:11 +0800

    微调【目录】

--- src/layout/menu.vue
+++ src/layout/menu.vue
@@ -1,12 +1,5 @@
 <template>
-  <a-menu
-    :openKeys="openKeys"
-    @openChange="onOpenChange"
-    :default-selected-keys="['1']"
-    :default-open-keys="['sub1']"
-    mode="inline"
-    theme="dark"
-  >
+  <a-menu :openKeys="openKeys" @openChange="onOpenChange" :default-selected-keys="['1']" :default-open-keys="['sub1']" mode="inline" theme="dark">
     <!-- <a-menu-item key="1">
           <a-icon type="pie-chart" />
           <span>Option 1</span>
@@ -18,8 +11,8 @@
     <a-sub-menu key="sub0">
       <span slot="title"><a-icon type="desktop" /><span>前端</span></span>
       <a-menu-item key="3">
-        <router-link to="/index"
-          ><a-icon type="inbox" />
+        <router-link to="/index">
+          <a-icon type="inbox" />
           <span>front-end</span>
         </router-link>
       </a-menu-item>
@@ -29,23 +22,11 @@
       <a-menu-item key="5">
         <router-link to="/index">首页</router-link>
       </a-menu-item>
-      <a-menu-item key="6">
-        Option 6
-      </a-menu-item>
-      <a-menu-item key="7">
-        Option 7
-      </a-menu-item>
-      <a-menu-item key="8">
-        Option 8
-      </a-menu-item>
     </a-sub-menu>
     <a-sub-menu key="sub2">
       <span slot="title"><a-icon type="appstore" /><span>css</span></span>
       <a-menu-item key="9">
-        Option 9
-      </a-menu-item>
-      <a-menu-item key="10">
-        Option 10
+        css
       </a-menu-item>
     </a-sub-menu>
     <a-sub-menu key="sub3">
@@ -53,9 +34,6 @@
       <a-sub-menu key="sub31" title="plugs">
         <a-menu-item key="11">
           <router-link to="/plugs/draggable">draggable</router-link>
-        </a-menu-item>
-        <a-menu-item key="12">
-          Option 12
         </a-menu-item>
       </a-sub-menu>
       <a-sub-menu key="sub32" title="vue">
